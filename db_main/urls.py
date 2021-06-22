1a9c4c8f7cdc10028c526d144b1b9b47265a7265
Igor <igoremin@gmail.com>
2021-06-22 14:21:48 +0300

    Добавил ссылку на файл с документацией

--- db_main/urls.py
+++ db_main/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -26,6 +27,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('feedback/', include('feedback.urls')),
+    path('documentation/', TemplateView.as_view(template_name='documentation.html'), name='doc_page_url'),
     path('', include('db_site.urls')),
 ]
 
