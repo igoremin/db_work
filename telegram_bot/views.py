5acf136eef79b865f09e0f8628a698fccd64a358
igor <igoremin@gmail.com>
2021-09-23 16:11:57 +0300

    Исправил логику работы, добавли проверку комменатриев на соответствие html формату телеграма

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -9,11 +9,45 @@ from django.utils.formats import date_format
 from django.conf import settings
 from db_site.views import home_page
 from django.shortcuts import redirect
+from html.parser import HTMLParser
 
 if not settings.DEBUG:
     apihelper.proxy = {'https': 'http://serv.sao.ru:8080'}
 
 bot = telebot.TeleBot(settings.TELEGRAM_KEY)
+
+
+class StringParser(HTMLParser):
+    def __init__(self, text):
+        self.support_tags = {
+            'b', 'strong', 'i', 'em', 'u', 'ins', 's', 'strike', 'del', 'a', 'code', 'pre'
+        }
+        self.text = text
+        super(StringParser, self).__init__()
+
+    def handle_starttag(self, tag, attrs):
+        if tag not in self.support_tags:
+            image_url = None
+
+            if tag == 'img':
+                for attr in attrs:
+                    if attr[0] == 'src':
+                        image_url = attr[1]
+            if attrs:
+                remove_str = f'<{tag}'
+                for attr in attrs:
+                    remove_str += f' {attr[0]}="{attr[1]}"'
+                remove_str += ' />'
+                if image_url:
+                    self.text = self.text.replace(remove_str, image_url)
+                else:
+                    self.text = self.text.replace(remove_str, '')
+            else:
+                self.text = self.text.replace(f'<{tag}>', '')
+
+    def handle_endtag(self, tag):
+        if tag not in self.support_tags:
+            self.text = self.text.replace(f'</{tag}>', '')
 
 
 def check_user(message):
@@ -30,27 +64,42 @@ def not_found_user_message(message):
            f"Ваш ID : {message.from_user.id}"
 
 
-def menu_for_task(message, task):
+def format_text_to_tg_html_style(text):
+    parser = StringParser(text=text)
+    parser.feed(text)
+    return parser.text
+
+
+def menu(message, task=None, call=False):
+    if call:
+        chat_id = message.message.chat.id
+    else:
+        chat_id = message.chat.id
+
     keyboard = types.InlineKeyboardMarkup(row_width=3)
-    comments = CommentForTask.objects.filter(task=task)
-    if len(comments) >= 1:
+    if task:
         key_read_comments = types.InlineKeyboardButton(
-            text='Посмотерть комментарии', callback_data=f'read_comments_for_task__{task.pk}'
+            text='Посмотреть комментарии', callback_data=f'read_comments_for_task__{task.pk}'
         )
-        keyboard.add(key_read_comments)
-    key_add_comment = types.InlineKeyboardButton(
-        text='Добавить комментарий', callback_data=f'add_comment_for_task__{task.pk}'
-    )
+        key_add_comment = types.InlineKeyboardButton(
+            text='Добавить комментарий', callback_data=f'add_comment_for_task__{task.pk}'
+        )
+        keyboard.add(key_read_comments, key_add_comment)
     key_task_list = types.InlineKeyboardButton(
         text='Все задачи', callback_data='tasks_list'
     )
     key_my_tasks = types.InlineKeyboardButton(
         text='Мои задачи', callback_data='tasks_list__for_user'
     )
-    keyboard.add(key_add_comment, key_task_list, key_my_tasks)
+    keyboard.add(key_task_list, key_my_tasks)
+
+    profile = Profile.objects.get(tg_id=message.from_user.id)
+    if profile.user.is_superuser:
+        key_change_lab = types.InlineKeyboardButton(text='Сменить лабораторию', callback_data='change_lab')
+        keyboard.add(key_change_lab)
 
     bot.send_message(
-        message.chat.id,
+        chat_id,
         text=f'Меню',
         reply_markup=keyboard,
         parse_mode='HTML'
@@ -58,52 +107,40 @@ def menu_for_task(message, task):
 
 
 @bot.message_handler(commands=['start', 'help'])
-def start_message(message):
-    if not check_user(message):
-        bot.reply_to(message, not_found_user_message(message))
-        return
-
+def start_message(message, call=False):
+    if not call:
+        if not check_user(message):
+            bot.reply_to(message, not_found_user_message(message))
+            return
     profile = Profile.objects.get(tg_id=message.from_user.id)
-    keyboard = types.InlineKeyboardMarkup(row_width=1)
-    key_tasks = types.InlineKeyboardButton(text='Мои задачи', callback_data='tasks_list__for_user')
-    key_all_tasks = types.InlineKeyboardButton(text='Все задачи для лаборатории', callback_data='tasks_list')
-    if profile.user.is_superuser:
-        key_change_lab = types.InlineKeyboardButton(text='Сменить лабораторию', callback_data='change_lab')
-        keyboard.add(key_change_lab, key_tasks, key_all_tasks)
-    else:
-        keyboard.add(key_tasks, key_all_tasks)
 
     lab = profile.tg_current_lab
     if profile.tg_current_lab is None:
-        Profile.objects.filter(tg_id=message.from_user.id).update(tg_current_lab=profile.lab.slug)
-        lab = profile.lab.slug
+        Profile.objects.filter(tg_id=message.from_user.id).update(tg_current_lab=profile.lab)
+        lab = profile.lab
+
+    if call:
+        chat_id = message.message.chat.id
+        message_id = message.message.message_id - 1
+        message = message.message
+    else:
+        chat_id = message.chat.id
+        message_id = message.message_id - 1
     try:
-        current_lab = LabName.objects.get(slug=lab)
-    except ObjectDoesNotExist:
-        current_lab = Profile.objects.get(tg_id=message.from_user.id).lab
+        bot.edit_message_reply_markup(
+            chat_id,
+            message_id=message_id,
+        )
+    except:
+        pass
 
     bot.send_message(
-        message.chat.id,
-        text=f'Главное меню\nТекущая лаборатория : {current_lab}',
-        reply_markup=keyboard,
-        parse_mode='HTML'
+        chat_id,
+        text=f'Добрый день!\nТекущая лаборатория : {lab.name}',
+        reply_markup=types.ReplyKeyboardRemove()
     )
 
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def change_lab(message):
-    if not check_user(message):
-        bot.reply_to(message, not_found_user_message(message))
-        return
-    new_lab = message.text
-    Profile.objects.filter(tg_id=message.from_user.id).update(tg_current_lab=new_lab)
-    profile = Profile.objects.get(tg_id=message.from_user.id)
-    bot.send_message(
-        chat_id=message.chat.id,
-        text=f'Пользователь {profile.name}.\nЛаборатория для отображения в телеграм боте : {profile.tg_current_lab}',
-        reply_markup=types.ReplyKeyboardRemove()
-        )
-    start_message(message)
+    menu(message)
 
 
 def run():
@@ -119,9 +156,9 @@ def tasks_for_lab(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     profile = Profile.objects.get(tg_id=message.from_user.id)
     if message.data == 'tasks_list':
-        all_tasks = Task.objects.filter(lab__slug=profile.tg_current_lab)
+        all_tasks = Task.objects.filter(lab=profile.tg_current_lab)
     else:
-        all_tasks = Task.objects.filter(lab__slug=profile.tg_current_lab, executors__exact=profile)
+        all_tasks = Task.objects.filter(executors__exact=profile)
     if len(all_tasks) > 0:
         for task in all_tasks:
             if task.create_date:
@@ -138,11 +175,13 @@ def tasks_for_lab(message):
         bot.register_next_step_handler(msg, task_info)
         bot.answer_callback_query(callback_query_id=message.id, text='')
     else:
+        bot.delete_message(chat_id=message.message.chat.id, message_id=message.message.message_id)
         bot.send_message(
             chat_id=message.message.chat.id,
-            text='Ничего не найдено!',
+            text=f'Задачи для лаборатории "{profile.tg_current_lab.name}" не найдены!',
         )
-        start_message(message)
+        bot.answer_callback_query(callback_query_id=message.id, text='')
+        start_message(message, call=True)
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
@@ -180,7 +219,7 @@ def task_info(message):
                 parse_mode='HTML',
                 reply_markup=types.ReplyKeyboardRemove(),
             )
-        menu_for_task(message, task)
+        menu(message, task)
     except ValueError:
         start_message(message)
 
@@ -204,7 +243,7 @@ def read_task_comments(call):
             name += ' <i>(робот)</i>'
         text += f'<strong>Автор</strong> : {name}\n'
         text += f'<strong>Дата</strong> : {date_format(comment.date)}\n'
-        text += f'<strong>Текст</strong> :\n{comment.get_message_as_markdown().replace("<p>", "").replace("</p>", "")}'
+        text += format_text_to_tg_html_style(comment.get_message_as_markdown())
         if len(text) > 4096:
             for x in range(0, len(text), 4096):
                 bot.send_message(
@@ -220,7 +259,7 @@ def read_task_comments(call):
             )
     if profile in task.new_comment_for_executors.all():
         task.new_comment_for_executors.remove(profile)
-    menu_for_task(call.message, task)
+    menu(call, task, call=True)
 
 
 @bot.callback_query_handler(func=lambda call: 'add_comment_for_task__' in call.data)
@@ -254,26 +293,46 @@ def save_new_comment_for_task(message, task_pk):
         chat_id=message.chat.id,
         text='Комментарий добавлен!',
     )
-    menu_for_task(message, task)
+    menu(message, task)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'change_lab')
 def callback_inline__change_lab(call):
     user = True
     if user:
-        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-        all_labs = LabName.objects.all().values_list('slug', flat=True)
+        # markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+        keyboard = types.InlineKeyboardMarkup(row_width=1)
+        all_labs = LabName.objects.all()
         if len(all_labs) > 0:
             for lab in all_labs:
-                markup.add(types.KeyboardButton(text=lab))
+                key_select_lab = types.InlineKeyboardButton(text=lab.name, callback_data=f'new_lab__{lab.slug}')
+                keyboard.add(key_select_lab)
+                # markup.add(types.KeyboardButton(text=lab))
             bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
-            msg = bot.send_message(
+            bot.send_message(
                 chat_id=call.message.chat.id,
                 text='Выберите лабораторию',
-                reply_markup=markup,
+                reply_markup=keyboard,
             )
-            bot.register_next_step_handler(msg, change_lab)
+            # bot.register_next_step_handler(msg, change_lab)
             bot.answer_callback_query(callback_query_id=call.id, text='')
+
+
+@bot.callback_query_handler(func=lambda call: 'new_lab__' in call.data)
+def change_lab(call):
+    if not check_user(call):
+        bot.reply_to(call, not_found_user_message(call))
+        return
+    new_lab = LabName.objects.get(slug=call.data.split('__')[-1])
+    Profile.objects.filter(tg_id=call.from_user.id).update(tg_current_lab=new_lab)
+    profile = Profile.objects.get(tg_id=call.from_user.id)
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+    bot.send_message(
+        chat_id=call.message.chat.id,
+        text=f'Пользователь {profile.name}.\nЛаборатория для отображения в телеграм боте : {new_lab.name}',
+    )
+    bot.answer_callback_query(callback_query_id=call.id, text='')
+    menu(call, call=True)
 
 
 def start_bot(request):
